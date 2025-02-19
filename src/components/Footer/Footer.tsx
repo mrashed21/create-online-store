4fb98393e51f28efaa87d71c71da2232ed44f614
mrashed21 <muhammadrashed760@gmail.com>
2025-02-19 19:11:01 +0600

    [ADD] added footer section

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,30 @@
 const Footer = () => {
-  return <div>this is footer section</div>;
+  return (
+    <footer className="bg-black">
+      <section className="container mx-auto py-8 text-white">
+        <div className="flex items-center justify-between">
+          <p>
+            © {new Date().getFullYear()} Create Online Store. All Rights
+            Reserved.
+          </p>
+          <div className="flex gap-2">
+            <a
+              href="#"
+              className="transition-colors hover:text-white dark:hover:text-gray-300"
+            >
+              Privacy Policy
+            </a>
+            <a
+              href="#"
+              className="transition-colors hover:text-white dark:hover:text-gray-300"
+            >
+              Terms & Conditions
+            </a>
+          </div>
+        </div>
+      </section>
+    </footer>
+  );
 };
 
 export default Footer;
